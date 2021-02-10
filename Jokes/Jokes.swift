b533691de77b7a0885c44fec6fd10b3ebe8b0742
ms2138 <m38371@gmail.com>
2021-02-09 21:29:13 -0800

    Add items property.  Add getJokes method to Jokes

--- Jokes/Jokes.swift
+++ Jokes/Jokes.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 class Jokes: ObservableObject {
+    @Published var items = [Joke]()
 
+    func getJokes() {
+        let url = URL(string: "https://icanhazdadjoke.com/")!
+        var urlRequest = URLRequest(url: url)
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if let data = data {
+                if let joke = try? JSONDecoder().decode(Joke.self,
+                                                        from: data) {
+                    DispatchQueue.main.async {
+                        self.items.insert(joke, at: 0)
+                    }
+                    return
+                }
+            }
+        }
+        task.resume()
+    }
 }
