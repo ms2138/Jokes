86a7b9b508085f1729458159f7f1ebae23c7372e
ms2138 <m38371@gmail.com>
2021-02-09 20:13:17 -0800

    Initial commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by mani on 2021-02-09.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
