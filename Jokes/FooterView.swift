cb192ba226629c9ec7ad15c725d56d81cb3d7435
ms2138 <m38371@gmail.com>
2021-02-13 21:49:46 -0800

    Add FooterView struct

---
+++ Jokes/FooterView.swift
@@ -0,0 +1,19 @@
+//
+//  FooterView.swift
+//  Jokes
+//
+//  Created by mani on 2021-02-13.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+    }
+}
