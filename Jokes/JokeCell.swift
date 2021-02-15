688845ba2bfa11d78e4aed5d91ebf503192bc1e3
ms2138 <m38371@gmail.com>
2021-02-14 20:01:01 -0800

    Add JokeCell struct

---
+++ Jokes/JokeCell.swift
@@ -0,0 +1,20 @@
+//
+//  JokeCell.swift
+//  Jokes
+//
+//  Created by mani on 2021-02-14.
+//
+
+import SwiftUI
+
+struct JokeCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JokeCell_Previews: PreviewProvider {
+    static var previews: some View {
+        JokeCell()
+    }
+}
