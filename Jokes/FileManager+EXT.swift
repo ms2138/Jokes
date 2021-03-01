dfd2ccc4e79e9d66acf732c0435d0493b19ebb76
ms2138 <m38371@gmail.com>
2021-02-28 23:20:48 -0800

    Add documentDirectory method to FileManager extension

--- Jokes/FileManager+EXT.swift
+++ Jokes/FileManager+EXT.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+extension FileManager {
+    var documentsDirectory: URL {
+        do {
+            return try FileManager.default.url(for: .documentDirectory,
+                                               in: .userDomainMask,
+                                               appropriateFor: nil,
+                                               create: false)
+        } catch {
+            fatalError("Failed to find documents directory.")
+        }
+    }
+}
