12734c914c5c29b27d8da93dc266452631acacef
ms2138 <m38371@gmail.com>
2021-03-08 21:01:14 -0800

    Add Codable protocol conformance to CodableBool.  Add extension to CodableBool to implement custom decoding and encoding methods

--- Jokes/CodableBool.swift
+++ Jokes/CodableBool.swift
@@ -8,6 +8,18 @@
 import Foundation
 
 @propertyWrapper
-struct CodableBool {
+struct CodableBool: Codable {
     var wrappedValue: Bool = false
+}
+
+extension CodableBool {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        wrappedValue = try container.decode(Bool.self)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(wrappedValue)
+    }
 }
